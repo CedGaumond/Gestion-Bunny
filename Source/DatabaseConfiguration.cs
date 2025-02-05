e2872222e57c9cd7aadedc493c20fb2a3d0fdfe3
CedGaumond <145615267+CedGaumond@users.noreply.github.com>
2025-02-05 13:12:16 -0800

    Update DatabaseConfiguration.cs

--- Source/DatabaseConfiguration.cs
+++ Source/DatabaseConfiguration.cs
@@ -6,7 +6,7 @@ public static class DatabaseConfiguration
             "Host=127.0.0.1;" + // Make sure the IP and port are correct
             "Port=5432;" +
             "Username=postgres;" + // Use the correct username
-            "Password=Lolipopi60;" + // Use the correct password
+            "Password=UlGBqeXlkG;" + // Use the correct password
             "Database=bunny_db;";  // Ensure the database exists and is accessible
 
         return databasePath;
