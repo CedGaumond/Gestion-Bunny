50c71b50f5ef69a35eda167987ec2fc3d4aed370
PyroGiirl <pyrogiirl@outlook.com>
2025-02-10 10:51:32 -0500

    Add BillProviderService.

---
+++ Source/Services/BillProviderService.cs
@@ -0,0 +1,52 @@
+﻿using Gestion_Bunny.Modeles;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Bunny.Services
+{
+    public class BillProviderService : IBillProviderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BillProviderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<BillProvider>> GetBillProvidersAsync()
+        {
+            return await _context.BillProviders.ToListAsync();
+        }
+
+        public async Task<BillProvider> GetBillProviderByIdAsync(int billProviderId)
+        {
+            return await _context.BillProviders.FindAsync(billProviderId);
+        }
+
+        public async Task AddBillProviderAsync(BillProvider billProvider)
+        {
+            await _context.BillProviders.AddAsync(billProvider);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBillProviderAsync(BillProvider billProvider)
+        {
+            _context.Entry(billProvider).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBillProviderAsync(int billProviderId)
+        {
+            var billProvider = await _context.BillProviders.FindAsync(billProviderId);
+            if (billProvider != null)
+            {
+                _context.BillProviders.Remove(billProvider);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Source/Services/IBillProviderService.cs
@@ -0,0 +1,18 @@
+﻿using Gestion_Bunny.Modeles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Bunny.Services
+{
+   public interface IBillProviderService
+   {
+        Task<List<BillProvider>> GetBillProvidersAsync();
+        Task<BillProvider> GetBillProviderByIdAsync(int billProviderId);
+        Task AddBillProviderAsync(BillProvider billProvider);
+        Task UpdateBillProviderAsync(BillProvider billProvider);
+        Task DeleteBillProviderAsync(int billProviderId);
+   }
+}
