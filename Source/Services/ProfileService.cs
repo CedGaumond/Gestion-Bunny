5ead7644f274b88e486b3c546d13e7d8bbb7f3bf
Jacob Lévesque <jaclev34@gmail.com>
2025-03-04 14:37:42 -0500

    Added the profile page and service

---
+++ Source/Services/ProfileService.cs
@@ -0,0 +1,32 @@
+using Gestion_Bunny.Modeles;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestion_Bunny.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProfileService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public RestaurantProfile GetProfile()
+        {
+            return _context.RestaurantProfiles.Find(1);
+        }
+
+        public void AddProfile(RestaurantProfile profileProvider)
+        {
+            _context.RestaurantProfiles.Add(profileProvider);
+            _context.SaveChanges();
+        }
+
+        public void UpdateProfile(RestaurantProfile profileProvider)
+        {
+            _context.Entry(profileProvider).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
